85496434507e0f91a02e37f923ba3ef147717b09
Tanyamodi <mtanu0126@gmail.com>
2024-07-15 13:48:15 +0530

    first commit

---
+++ DAY_1/01_FindDuplicateNUmber.cpp
@@ -0,0 +1,21 @@
+/*
+287. Find The Duplicate Number
+https://leetcode.com/problems/find-the-duplicate-number/description/
+*/
+
+class Solution
+{
+public:
+    int findDuplicate(vector<int> &nums)
+    {
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size() - 1; i++)
+        {
+            if (nums[i] == nums[i + 1])
+            {
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+};
